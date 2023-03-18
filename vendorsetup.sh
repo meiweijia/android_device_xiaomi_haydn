0bc73a8b2f5cd966a52a03edbf9cd55e1b19d1fe
meiwj <meiweijia@gmail.com>
2023-03-18 15:43:10 +0800

    vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+
+# Kernel tree
+git clone --depth=1 https://github.com/LineageOS/android_kernel_xiaomi_sm8350.git kernel/xiaomi/sm8350
+
+# Vendor tree
+git clone --depth=1 https://gitlab.com/meiweijia/vendor_xiaomi_haydn.git -b thirteen vendor/xiaomi/haydn
+
+# Firmware
+git clone https://gitlab.com/meiweijia/vendor_xiaomi_haydn-firmware.git -b thirteen vendor/xiaomi/haydn-firmware
+
+# Hardware xiaomi
+git clone https://github.com/PixelExperience/hardware_xiaomi.git -b thirteen hardware/xiaomi 
