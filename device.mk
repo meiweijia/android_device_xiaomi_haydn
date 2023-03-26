68e40fa457dbc3215d6942e42e979b0633b39add
meiwj <meiweijia@gmail.com>
2023-03-26 21:42:42 +0800

    sm8350-common: Build android.frameworks.sensorservice@1.0 HAL

--- device.mk
+++ device.mk
@@ -379,7 +379,9 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.haydn-multihal
+    android.hardware.sensors@2.1-service.haydn-multihal \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
