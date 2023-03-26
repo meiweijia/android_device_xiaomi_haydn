45fe0f916552d839718247cefd705c0e7f946aeb
meiwj <meiweijia@gmail.com>
2023-03-26 22:42:59 +0800

    import vendor task_profiles.json from MIUI V14.0.3.0.TKKCNXM

--- device.mk
+++ device.mk
@@ -359,7 +359,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Perf
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
-    
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
