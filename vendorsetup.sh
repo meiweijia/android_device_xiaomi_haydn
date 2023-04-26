43bca07b067ed45b60b344a7d2bac3f55eaa262e
meiwj <meiweijia@gmail.com>
2023-04-26 17:21:07 +0800

    haydn: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,22 +6,22 @@ git clone https://github.com/meiweijia/android_device_xiaomi_sm8350-common.git -
 
 # Kernel
 echo 'Cloning kernel tree [2/6]'
-git clone --depth=1 https://github.com/Lineageos/android_kernel_xiaomi_sm8350 -b lineage-19.1 kernel/xiaomi/sm8350
+git clone --depth=1 https://github.com/Lineageos/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350
 
 # Vendor
 echo 'Cloning vendor tree [3/6]'
-git clone https://github.com/AdarshGrewal/proprietary_vendor_xiaomi_haydn.git -b lineage-20 vendor/xiaomi/haydn
+git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_haydn.git -b lineage-20 vendor/xiaomi/haydn
 
 # Vendor common
 echo 'Cloning common vendor tree [4/6]'
-git clone https://github.com/AdarshGrewal/proprietary_vendor_xiaomi_sm8350-common.git -b lineage-20 vendor/xiaomi/sm8350-common
+git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common.git -b lineage-20 vendor/xiaomi/sm8350-common
 
 # Firmware
 echo 'Cloning firmware [5/6]'
-git clone https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn-firmware -b thirteen vendor/xiaomi/haydn-firmware
+git clone https://gitlab.com/meiweijia/vendor_xiaomi_haydn-firmware.git -b thirteen vendor/xiaomi/haydn-firmware
 
-# Xiaomi
-echo 'Cloning hardware xiaomi [6/6]'
-rm -rf hardware/xiaomi && git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+# MIUI Camera
+echo 'Cloning Leica Camera [6/6]'
+git clone https://gitlab.com/AdarshGrewal/vendor_xiaomi_haydn-miuicamera.git -b lineage-20 vendor/xiaomi/haydn-miuicamera
 
 echo 'delete vendorsetup.sh from device tree once this is done'
